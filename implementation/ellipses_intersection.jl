5c8f9d2bff512e09404d96655ec8912e6e5e908e
Danilo Tedeschi <danilof.t2@gmail.com>
2019-04-24 16:43:00 -0300

    WIP

--- implementation/ellipses_intersection.jl
+++ implementation/ellipses_intersection.jl
@@ -21,16 +21,20 @@ function intersection(e_1::Ellipse, e_2::Ellipse)::Union{Nothing,Tuple{Point,Poi
 	a::Real = e_1.a
 	b::Real = e_1.b
 
-	α = (-2k*a^2)/(2h*b^2)
-	β = (b^2*h^2 + a^2*k^2) / (2*h*b^2)
+	α = (-2k*a²)/(2h*b²)
+	β = (b²h² + a²k²) / (2h*b²)
 	
-	ro = root2(b^2*α^2+a^2, 2*β*α*b^2, b^2*β^2 - a^2*b^2)
+	ro = root2(b²α²+a², 2β*α*b², b²*β² - a²*b²)
 
 	if (ro === nothing)
 		return nothing 
 	end
 	
 	y_1, y_2 = ro
+
+	@show(y_1, y_2)
+	@show(b²α²+a², 2β*α*b², b²*β² - a²*b²)
+
 
 	x_1 = y_1*α + β
 	x_2 = y_2*α + β
